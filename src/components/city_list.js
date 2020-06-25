a0c7d7303459de0c3dd85d4e1cd42e7fff29a83e
phandeyr <phandeyr@gmail.com>
2020-06-25 19:27:10 +0100

    invokes openweathermap api which requires lon and lat, updated hardcoded city list to include lon and lat and pass these properties to city weather component, started to display current weather data (wip)

--- src/components/city_list.js
+++ src/components/city_list.js
@@ -4,18 +4,24 @@ import 'semantic-ui-css/semantic.min.css'
 
 class CityList extends Component {
   render () {
-    const cities = ['Wolverhampton', 'Birmingham', 'Manchester', 'London']
+    // Hard-coded array of cities with their corresponding latitude and longitude
+    const cities = [
+      { city: 'Wolverhampton', 'lat': 52.586971, 'lon': -2.128820 },
+      { city: 'Birmingham', 'lat': 33.518589, 'lon': -86.810356 },
+      { city: 'Manchester', 'lat': 53.480759, 'lon': -2.242631 },
+      { city: 'London', 'lat': 51.507351, 'lon': -0.127758 }
+    ]
 
     return (
       <div className='city'>
         <List divided verticalAlign='middle'>
           {cities.map((city, index) =>
-            <List.Item key={index} onClick={() => this.props.history.push(`/weather/${city}`)}>
+            <List.Item key={index} onClick={() => this.props.history.push(`/weather/${city.city}`, { data: city } )}>
               <List.Content floated='right'>
                 <Icon className='chevron right icon'/>
               </List.Content>
               <List.Content>
-                <List.Header>{city}</List.Header>
+                <List.Header>{city.city}</List.Header>
                 Great Britain, UK
               </List.Content>
             </List.Item>
--- src/components/city_weather.js
+++ src/components/city_weather.js
@@ -1,9 +1,86 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
+import config from '../config.js'
+import { Icon } from 'semantic-ui-react'
 
 class CityWeather extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      data: props.location.state.data,
+      results: []
+    }
+  }
+
+  componentDidMount () {
+    this.getWeather()
+  }
+
+  /**
+   * Invokes openweathermap api to retrieve weather results
+   */
+  getWeather () {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.data.lat}&lon=${this.state.data.lon}&exclude=minutely&appid=${config.API_KEY}`)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          results: data,
+          isLoading: false
+        })
+      })
+      .catch(console.log)
+  }
+
+  /**
+   * Converts temp from kelvin to celcius and returns it
+   * @param {int} temp Integer value to convert
+   */
+  convertKelvinToCelcius (temp) {
+    temp = temp - 273.15
+    return parseInt(temp)
+  }
+
+  /**
+   * Converts timestamp to date and returns the formatted date or time
+   * @param {int} timestamp
+   */
+  formatDateTime (timestamp, option) {
+    const days = ['Sunday', 'Monday', 'Tuesdy', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    const dateMS = new Date(timestamp * 1000)
+    const weekday = dateMS.getDay()
+    const date = dateMS.getDate()
+    const month = dateMS.getMonth()
+    const year = dateMS.getFullYear()
+
+    if (option === 'date') {
+      return `${days[weekday]}, ${date} ${months[month]} ${year}`
+    } else {
+      return dateMS.getHours() + ':' + dateMS.getMinutes()
+    }
+  }
+
   render () {
-    const city = this.props.match.params.city
-    return `City ${city}`
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <Icon className='spinner icon' size='big'/><br/>
+          <p>Loading</p>
+        </div>
+      )
+    }
+
+    const current = this.state.results.current
+    return (
+      <div className='current_weather'>
+        <h3>{this.formatDateTime(current.dt, 'date')}</h3>
+        <h1>{this.formatDateTime(current.dt, 'time')}</h1>
+        <h3>{this.state.data.city}</h3>
+        <h4>{current.weather[0].main}</h4>
+        <p id='temp'>{this.convertKelvinToCelcius(current.temp)}&deg;C</p>
+      </div>
+    )
   }
 }
 
